d9a1d9e606e9d4c9351cd9d70af50d35b27be2a4
Dawoot <david.rosenqvist4@gmail.com>
2025-05-31 17:46:30 +0200

    Added map functionality and a different movement system

--- main.c
+++ main.c
@@ -1,79 +1,123 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "raylib.h"
+#define _GNU_SOURCE
 
 #define FPS 144
-#define window_width 800
-#define window_height 600
+#define window_width 1920
+#define window_height 1080
+
+int **map;
+
+void initializetiledmap(int gridwidth, int gridheight){
+    map = (int **)malloc(gridheight*sizeof(int*));
+    if (map == NULL) {
+        printf("Bro something went wrong when initalizing memory for map\n");
+    }
+    for (int i = 0; i<gridheight ; i++) {
+        map[i] = (int*)malloc(gridwidth*sizeof(int*));
+        if (map[i] == NULL) {
+        printf("Something went wrong initalizing the memory of y map\n");
+        }
+    for (int x =0 ; x<gridwidth; x++) {
+
+    map[i][x] = 0;
+        }
+    }
+}
+void loadmap(char *filename, int gridwidth, int gridheight){
+    FILE *file;
+    file = fopen(filename, "rb");
+    if (file == NULL) {
+    printf("Error loadingmap\n");
+        return;
+    }
+    int localwidth, localheight;
+    fread(&localwidth, sizeof(int), 1, file);
+    fread(&localheight, sizeof(int), 1, file);
+    if (localheight != gridheight || localwidth != gridwidth) {
+        printf("\nSomething went wrong here!\n");
+    }
+    for (int i = 0; i<gridheight; i++) {
+    fread(map[i], sizeof(int), gridwidth, file);    
+    }
+    for (int y = 0; y<gridheight; y++) {
+    for (int x = 0; x<gridwidth; x++) {
+            printf("Mapvalue at: y:%d x:%d  value:%d\n", y,x,map[y][x]);
+    }
+    }
+    fclose(file);
+}
+void freemapmem(int gridwidth, int gridheight){
+    for (int y = 0; y<gridheight; y++) {
+    free(map[y]);
+    }
+    free(map);
+}
 
 int main(){
     Texture2D player;
+    int blocksize = 100;
+    int gridwidth = window_width/blocksize;
+    int gridheight = (window_height-100)/blocksize;
+    initializetiledmap(gridwidth, gridheight);
     InitWindow(window_width, window_height, "Love to amela");
+    loadmap("map.bin", gridwidth, gridheight);
+    Color palette[] = {BLUE, BROWN, RED, GREEN, PURPLE};
     player = LoadTexture("textures/player.png");
     SetTargetFPS(FPS);
-    
-    int speed_x, speed_y = 0;
-    int playerpos_y = 200;
-    int playerpos_x = 200;
-    int player_width = (window_width/2)-250;
-    int player_height = (window_height/2) -250;
+    UnloadTexture(player);
+    int playerpos_y = 100;
+    int playerpos_x = 100;
 
-    Camera2D player_camera = {0};
-
-    player_camera.target = (Vector2){player_width + 20.0f, player_height + 20.0f};
-    player_camera.offset = (Vector2){window_width/2.0f, window_height/2.0f};
-    player_camera.rotation = 0.0f;
 
     while (!WindowShouldClose()) {
         //For movement
         float dt = GetFrameTime();
-        if (IsKeyDown(KEY_D)) {
-            speed_x = 10 + dt;
-        playerpos_x = speed_x + playerpos_x;
-        playerpos_y = speed_y + playerpos_y;
+        if (IsKeyPressed(KEY_S)) {
+            playerpos_y = playerpos_y+100;
         }
-        if (IsKeyDown(KEY_S)) {
-            speed_y = 10 + dt;
-        playerpos_x = speed_x + playerpos_x;
-        playerpos_y = speed_y + playerpos_y;
+        if (IsKeyPressed(KEY_A)) {
+            playerpos_x = playerpos_x -100;
         }
-        if (IsKeyDown(KEY_A)) {
-        speed_x = -10 -dt;
-        playerpos_x = speed_x + playerpos_x;
-        playerpos_y = speed_y + playerpos_y;
+        if (IsKeyPressed(KEY_D)) {
+        playerpos_x = playerpos_x+100;
         }
-        if (IsKeyDown(KEY_W)) {
-        speed_y = -10-dt;
-        playerpos_x = speed_x + playerpos_x;
-        playerpos_y = speed_y + playerpos_y;
+        if (IsKeyPressed(KEY_W)) {
+        playerpos_y = playerpos_y-100;
         }
-        if (playerpos_x+player.width>window_width || playerpos_x <0) {
-            speed_x = speed_x *-1.2;
-        playerpos_x = speed_x + playerpos_x;
-        playerpos_y = speed_y + playerpos_y;
+        if (playerpos_y<0) {
+        playerpos_y = 0;
+        playerpos_x = playerpos_x;
         }
-        if (playerpos_y+player.height>window_height || playerpos_y < 0) {
-            speed_y = speed_y *-1.2;
-        playerpos_x = speed_x + playerpos_x;
-        playerpos_y = speed_y + playerpos_y;
+        if (playerpos_y+player.height>window_height) {
+            playerpos_y = window_height - player.height;
         }
-        else {
-        speed_y = 0;
-        speed_x = 0;
+        if (playerpos_x<0) {
+        playerpos_x = 0;
+            playerpos_y = playerpos_y;
+        }
+        if (playerpos_x + player.width > window_width) {
+        playerpos_x = window_width - player.width;
+        }
 
-        playerpos_x = speed_x + playerpos_x;
-        playerpos_y = speed_y + playerpos_y;
-        }
         //Camera
-        player_camera.target = (Vector2){player_width + 20.0f, player_height + 20.0f};
         //updating drawing
         BeginDrawing();
-        ClearBackground(RED);
+        ClearBackground(RAYWHITE);
+        for (int y = 0; y<gridheight; y++) {
+        for (int x = 0; x<gridwidth; x++) {
+                if (map[y][x] > 0) {
+                DrawRectangle(x * blocksize, y*blocksize+100, blocksize,blocksize, palette[map[y][x] - 1]);
+                }
+                DrawRectangleLines(x* blocksize, y* blocksize+100, blocksize, blocksize, GRAY);
+            }
+        }
         DrawTexture(player, playerpos_x, playerpos_y, BLACK);
-        BeginMode2D(player_camera); 
-        EndMode2D();
+        
         EndDrawing();
     }
-    UnloadTexture(player);
+    freemapmem(gridwidth, gridheight);
     CloseWindow();
     return 0;
 }
